30d70b3a7d0f1cd7df89d8b5a0a2683583153024
francisco <francisco.infanti@gmail.com>
2023-11-17 20:04:47 -0300

    Empiezo con el main

--- main.c
+++ main.c
@@ -1,78 +1,194 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
 #include "src/pokemon.h"
 #include "src/ataque.h"
 #include "src/juego.h"
 #include "src/lista.h"
 #include "src/adversario.h"
-#include <stdio.h>
 
-/**
-* Este main debe ser modificado para que el usuario pueda jugar el juego. Las
-* instrucciones existentes son solamente a modo ilustrativo del funcionamiento
-* muy alto nivel del juego.
-*
-* Las interacciones deben realizarse por entrada/salida estandar y estar validadas.
-*
-* Se aconseja en todo momento mostrar información relevante para el jugador (por
-* ejemplo, mostrar puntaje actual y movimientos disponibles) para hacer que el
-* juego sea facil de utilizar.
-*/
+#define BLANCO "\x1b[37;1m"
+#define VERDE "\x1b[32;1m"
+#define ROJO "\x1b[31;1m"
+#define AMARILLO "\x1b[33;1m"
+#define NORMAL "\x1b[0m"
+#define MAGNETA "\x1b[35;1m"
+
+enum RESULTADO { ERROR, OK, SALIR };
+
+#define CMD_AYUDA "ayuda"
+#define CMD_SALIR "salir"
+#define CMD_SELECCIONAR_POKEMONES "s"
+#define CMD_HACER_JUGADA "j"
+#define CMD_MOSTRAR_PUNTAJE "p"
+#define MAX_CARACTERES 50
+
+void informar_aviso(char *aviso, bool error)
+{
+	error ? printf(ROJO "ERROR: " NORMAL) : printf(VERDE "EXITO: " NORMAL);
+	printf("%s\n", aviso);		
+}
+
+void mostrar_comando(char *comando, char *descripcion)
+{
+	printf(AMARILLO "%s: ", comando);
+	printf(NORMAL "%s\n", descripcion);
+}
+
+enum RESULTADO mostrar_comandos_disponibles()
+{
+	printf("Tenes disponibles los siguientes comandos: \n");
+	mostrar_comando(CMD_AYUDA, "Muestra por pantalla los comandos disponibles");
+	mostrar_comando(CMD_SALIR, "Sale del programa");
+	mostrar_comando(CMD_SELECCIONAR_POKEMONES, "Selecciona los pokemones para comenzar a jugar");
+	mostrar_comando(CMD_HACER_JUGADA, "Selecciona el pokemon y ataque para poder realizar una jugada");
+	mostrar_comando(CMD_MOSTRAR_PUNTAJE, "Muestra el puntaje de los jugadores");
+	return OK;
+}
+
+void mostrar_ataques_disponibles(const struct ataque *ataque, void *aux)
+{
+	printf(MAGNETA "Poder: ");
+	printf(NORMAL "%u\t", ataque->poder);
+	printf(ROJO "\t Ataque: ");
+	printf(NORMAL "%s\n", ataque->nombre);
+}
+
+bool mostrar_pokemones_disponibles(void *_pokemon, void *aux)
+{
+	printf(AMARILLO "Nombre: ");
+	printf(NORMAL "%s\n", pokemon_nombre(_pokemon));
+	con_cada_ataque(_pokemon, mostrar_ataques_disponibles, aux);
+	return true;
+}
+
+void listar_pokemones(void *juego)
+{
+        lista_con_cada_elemento(juego_listar_pokemon((juego_t *)juego), mostrar_pokemones_disponibles, NULL);
+}
+
+enum RESULTADO seleccionar_pokemones_usuario(juego_t *juego)
+{
+        printf("Tenes todos estos pokemones, selecciona tres. Los dos primeros son para vos y el tercero para tu adversario.\n");
+	listar_pokemones(juego);
+
+        JUEGO_ESTADO estado = TODO_OK;
+	char nombre1[MAX_CARACTERES];
+	char nombre2[MAX_CARACTERES];
+	char nombre3[MAX_CARACTERES];
+	char *nombres[] = { nombre1, nombre2, nombre3 };
+
+	for (int i = 0; i < 3; i++) {
+		printf(MAGNETA "==TP2== " NORMAL);
+		printf("Nombre: ");
+		fscanf(stdin, "%s", nombres[i]);
+	}
+
+	estado = juego_seleccionar_pokemon((juego_t *)juego, JUGADOR1, nombre1, nombre2, nombre3);
+
+        if (estado == POKEMON_INEXISTENTE || estado == POKEMON_REPETIDO) {
+		informar_aviso(estado == POKEMON_INEXISTENTE ? "Ingresa pokemones de la lista" : "No se pueden pokemones repetidos", true);
+                return seleccionar_pokemones_usuario(juego);
+        }
+        
+	if (estado == ERROR_GENERAL) {
+		informar_aviso("No es tu culpa", true);
+		return ERROR;
+	}
+
+	informar_aviso(":)", false);
+	return OK;
+}
+
+enum RESULTADO seleccionar_pokemones_ia(juego_t *juego, adversario_t *ia)
+{
+	JUEGO_ESTADO estado = TODO_OK;
+	char *nombre1;
+	char *nombre2;
+	char *nombre3;
+	
+	adversario_seleccionar_pokemon(ia, &nombre1, &nombre2, &nombre3);
+	
+	estado = juego_seleccionar_pokemon(juego, JUGADOR2, (const char *)nombre1, (const char *)nombre2, (const char *)nombre3);
+	if (estado == POKEMON_REPETIDO)
+		return seleccionar_pokemones_ia(juego, ia);
+	
+	return estado;
+}
+
+enum RESULTADO mostrar_puntaje(juego_t *juego)
+{
+	printf(AMARILLO "Usuario: " NORMAL);
+	printf("%i\n", juego_obtener_puntaje(juego, JUGADOR1));
+	printf(AMARILLO "Ia: " NORMAL);
+	printf("%i\n", juego_obtener_puntaje(juego, JUGADOR2));
+	return OK;
+}
+
+enum RESULTADO ejecutar_comando(char *comando, juego_t *juego, adversario_t *ia)
+{
+	if (strcmp(comando, CMD_AYUDA) == 0)
+		return mostrar_comandos_disponibles();
+
+	if (strcmp(comando, CMD_SALIR) == 0)
+		return SALIR;
+		
+	if (strcmp(comando, CMD_SELECCIONAR_POKEMONES) == 0) 
+		return seleccionar_pokemones_usuario(juego) && seleccionar_pokemones_ia(juego, ia);
+		
+//	if (strcmp(comando, CMD_HACER_JUGADA) == 0)
+//		return realizar_jugada_usuario(juego) && realizar_jugada_ia(juego, ia);
+
+	if (strcmp(comando, CMD_MOSTRAR_PUNTAJE) == 0)
+		return mostrar_puntaje(juego);
+	
+	informar_aviso(("El comando no existe, intenta con 'ayuda'"), true);
+	return ERROR;
+}
+
+void liberar_todo(juego_t *juego, adversario_t *ia)
+{
+        adversario_destruir(ia);
+        juego_destruir(juego);        
+}
 
 int main(int argc, char *argv[])
 {
-	juego_t *juego = juego_crear();
+	if (argc != 2) {
+		informar_aviso("Ingrese el nombre del archivo de pokemones :)", true);
+        	return -1;
+      	}
 
-	//Pide al usuario un nombre de archivo de pokemones
-	char *archivo = pedir_archivo();
+        juego_t *juego = juego_crear();
+	if (!juego) 
+		return -1;
 
-	//Carga los pokemon
-	juego_cargar_pokemon(juego, archivo);
+	JUEGO_ESTADO estado = juego_cargar_pokemon(juego, *(argv + 1));
+        if (estado != TODO_OK) {
+		informar_aviso(estado == ERROR_GENERAL ? "error el archivo no existe" : "La cantidad de pokemones es invalida, intenta con otro archivo", true);
+                liberar_todo(juego, NULL);
+                return -1;
+        }
 
-	//Crea un adversario que será utilizado como jugador 2
-	adversario_t *adversario =
-		adversario_crear(juego_listar_pokemon(juego));
+	adversario_t *ia = adversario_crear(juego_listar_pokemon(juego));
+	if (!ia) {
+		liberar_todo(juego, NULL);
+		return -1;
+	}
+ 
+	printf("Ingrese 'ayuda' para ver los comandos disponibles\n");
 
-	//Mostrar el listado de pokemones por consola para que el usuario sepa las opciones que tiene
-	mostrar_pokemon_disponibles(juego);
+	enum RESULTADO resultado = OK;
+	while (!juego_finalizado(juego) && resultado != SALIR) {
+		printf(MAGNETA "==TP2== " NORMAL);
+		char comando[MAX_CARACTERES];
+		fscanf(stdin, "%s",comando);
 
-	//Pedirle al jugador por consola que ingrese los 3 nombres de pokemon que quiere utilizar
-	char *eleccionJugador1, *eleccionJugador2, *eleccionJugador3;
-	jugador_seleccionar_pokemon(juego, &eleccion_adversario1,
-				    &eleccion_adversario2,
-				    &eleccion_adversario3);
-
-	//pedirle al adversario que indique los 3 pokemon que quiere usar
-	char *eleccionAdversario1, *eleccionAdversario2, *eleccionAdversario3;
-	adversario_seleccionar_pokemon(adversario, &eleccionAdversario1,
-				       &eleccionAdversario2,
-				       &eleccionAdversario3);
-
-	//Seleccionar los pokemon de los jugadores
-	juego_seleccionar_pokemon(juego, JUGADOR1, eleccionJugador1,
-				  eleccionJugador2, eleccionJugador3);
-	juego_seleccionar_pokemon(juego, JUGADOR2, eleccionAdversario1,
-				  eleccionAdversario2, eleccionAdversario3);
-
-	//informarle al adversario cuales son los pokemon del jugador
-	adversario_pokemon_seleccionado(adversario, eleccionJugador1,
-					eleccionJugador2, eleccionJugador3);
-
-	while (!juego_finalizado(juego)) {
-		resultado_jugada_t resultado_ronda;
-
-		//Pide al jugador que ingrese por consola el pokemon y ataque para la siguiente ronda
-		jugada_t jugada_jugador = jugador_pedir_nombre_y_ataque();
-
-		//Pide al adversario que informe el pokemon y ataque para la siguiente ronda
-		jugada_t jugada_adversario =
-			adversario_proxima_jugada(adversario);
-
-		//jugar la ronda y después comprobar que esté todo ok, si no, volver a pedir la jugada del jugador
-		resultado_ronda = juego_jugar_turno(juego, jugada_jugador,
-						    jugada_adversario);
-
-		//Si se pudo jugar el turno, le informo al adversario la jugada realizada por el jugador
-		adversario_informar_jugada(adversario, jugada_jugador);
+		resultado = ejecutar_comando(comando, juego, ia);
 	}
 
-	juego_destruir(juego);
+        liberar_todo(juego, ia);
+        return 0;
 }
